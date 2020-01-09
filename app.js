036aa04ce342f4ccdb566f2f628e4bfe49a27aa7
Adnan Hakim <adnanhakim007@gmail.com>
2020-01-09 21:13:42 +0530

    Removed code

--- app.js
+++ app.js
@@ -12,35 +12,27 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Import Routes
-const postRoute = require('./routes/posts');
 const questionRoute = require('./routes/questions');
-const authRoute = require('./routes/auth');
 
 // Routes Middlewares
-app.use('/posts', postRoute);
-app.use('/auth', authRoute);
 app.use('/api', questionRoute);
 
 // Routes
 app.get('/', (req, res) => {
-   res.sendFile(__dirname + '/templates/index.html');
+    res.sendFile(__dirname + '/templates/index.html');
 });
 
 app.get('/play', (req, res) => {
-   res.sendFile(__dirname + '/templates/play.html');
+    res.sendFile(__dirname + '/templates/play.html');
 });
 
 app.get('/api/question', (req, res) => {
-   res.sendFile(__dirname + '/templates/addquestion.html');
-});
-
-app.get('/chart', (req, res) => {
-   res.sendFile(__dirname + '/templates/temp.html');
+    res.sendFile(__dirname + '/templates/addquestion.html');
 });
 
 // Connect to db
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () =>
-   console.log('Connected to DB')
+    console.log('Connected to DB')
 );
 
 // Listen to server
